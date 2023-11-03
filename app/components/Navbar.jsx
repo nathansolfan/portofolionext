a6f232bb69629f4d05f82558320031a6e038132d
nathansolfan <nathansolfan@hotmail.com>
2023-11-03 19:28:04 +0000

    Portfolio website

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,16 @@
+export default function Navbar() {
+  return (
+    <nav>
+    <div className="logo">
+      {/* Add your logo or site name here */}
+      <h1>Site Name</h1>
+    </div>
+    <ul>
+      <li><a href="/">Home</a></li>
+      <li><a href="/about">About</a></li>
+      <li><a href="/work">Work</a></li>
+      <li><a href="/contact">Contact</a></li>
+    </ul>
+  </nav>
+  )
+}
