0a000ee6ea618ff60e68022374cbd70c2d4f5ffb
nathansolfan <nathansolfan@hotmail.com>
2023-11-07 09:25:25 +0000

    Hover effect on paragraphs. p:hover{}

--- app/dashboard/about/about.jsx
+++ app/dashboard/about/about.jsx
@@ -5,7 +5,7 @@ export default function AboutMe() {
   return (
     <section>
       <h1>About Me</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ut elit in neque faucibus interdum eget eget urna. Phasellus vulputate enim vel mauris sodales, a lobortis est sollicitudin. Aenean in magna id libero cursus maximus sed sit amet arcu. Nullam vel felis in ligula efficitur facilisis. Ut placerat orci quis risus vulputate, ut fermentum dui blandit. Vivamus lacinia feugiat nunc, eget fermentum justo sollicitudin quis.</p>
+      <p >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ut elit in neque faucibus interdum eget eget urna. Phasellus vulputate enim vel mauris sodales, a lobortis est sollicitudin. Aenean in magna id libero cursus maximus sed sit amet arcu. Nullam vel felis in ligula efficitur facilisis. Ut placerat orci quis risus vulputate, ut fermentum dui blandit. Vivamus lacinia feugiat nunc, eget fermentum justo sollicitudin quis.</p>
     </section>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,11 @@ export default function Home() {
       <main>
         <h2>Portfolio</h2>
         {/* Rest of your page content */}
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ut elit
+          in neque faucibus interdum eget eget urna. Phasellus vulputate enim
+          vel mauris sodales
+        </p>
       </main>
     </>
   );
