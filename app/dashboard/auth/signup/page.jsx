04d4f68389cc99a76527050122be7e2e5020cf4b
nathansolfan <nathansolfan@hotmail.com>
2023-11-07 10:11:59 +0000

    Working form. Next step is handling the click event

---
+++ app/dashboard/auth/signup/page.jsx
@@ -0,0 +1,23 @@
+// app/dashboard/tickets/create/page.jsx
+import React from 'react';
+import AuthComponent from './signup.jsx'; // Adjust the import path as needed
+
+
+
+export default function AboutMeDisplay() {
+  return (
+    <>
+      <main>
+        <h2>Portfolio</h2>
+        {/* Rest of your page content */}
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ut elit
+          in neque faucibus interdum eget eget urna. Phasellus vulputate enim
+          vel mauris sodales
+        </p>
+        {/* Include the Auth component */}
+        <AuthComponent />
+      </main>
+    </>
+  );
+}
--- app/dashboard/auth/signup/signup.jsx
+++ app/dashboard/auth/signup/signup.jsx
@@ -1,9 +1,40 @@
-import { createClient } from '@supabase/supabase-js'
-import { Auth } from '@supabase/auth-ui-react'
+// since its not a server component, we have to add this line of code to make it client
+"use client"
+import React, { useState } from 'react'
 
-const supabase = createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-    )
+// Import useState 
+// Get the handleSubmit() prop and use it on the AuthForm()
+export default function AuthForm( {handleSubmit} ) {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
 
-const App = () => <Auth supabaseClient={supabase} />
+  return (
+    // The form itself - label and span - add btn at end
+    // Again (e) is the event from the form submission
+    <form onSubmit={(e) => handleSubmit(e, email, password )}>
+        <label>
+            <span>Email:</span>
+            <input
+            type="text"
+            // the onChange={ has a arrow function}
+            // = () => setEmail()
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+            required
+            />
+        </label>
+        {/* Password Form */}
+        <label>
+            <span>Password:</span>
+            <input 
+            type="password"
+            onChange={(e)=> setPassword(e.target.value)} 
+            value={password}
+            required
+            />
+        </label>
+        {/* When pressed we have access to the Event (e) on the onSubmit() */}
+        <button className='btn-primary'>Submit</button>
+    </form>
+  )
+}
