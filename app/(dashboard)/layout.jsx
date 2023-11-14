59d7abefbab6e9cacae4f47d55ce733a59d0194b
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2023-11-14 00:12:50 +0000

    Fixed layout

--- app/(dashboard)/layout.jsx
+++ app/(dashboard)/layout.jsx
@@ -1,8 +1,21 @@
-import Navbar from "../components/Navbar"
+// app/dashboard/layout.tsx
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+import Navbar from "../components/Navbar";
 
-export default function DashboardLayout({ children }) {
+export default async function DashboardLayout({ children }) {
+  const supabase = createServerComponentClient({ cookies });
+  const { data } = await supabase.auth.getSession();
+
+  if (!data.session) {
+    console.log("Redirecting to login");
+    redirect("/login");
+  }
+
   return (
     <>
+      <Navbar user={data.session.user} />
       {children}
     </>
   );
